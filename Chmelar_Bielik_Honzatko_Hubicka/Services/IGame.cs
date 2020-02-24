1c01d0a2d7dbb77520e2811216d29556bce21150
Tomáš Chmelař <u17.410@pslib.cloud>
2020-02-24 12:19:31 +0100

    Commit

---
+++ Chmelar_Bielik_Honzatko_Hubicka/Services/IGame.cs
@@ -0,0 +1,17 @@
+﻿using Chmelar_Bielik_Honzatko_Hubicka.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chmelar_Bielik_Honzatko_Hubicka.Service
+{
+    interface IGame
+    {
+        bool AddPlayer(User User);
+        bool AddGame(Game Game);
+        bool RemoveGame(Game Game);
+        bool PieceNow(NavyBattlePiece Piece);
+        
+    }
+}
