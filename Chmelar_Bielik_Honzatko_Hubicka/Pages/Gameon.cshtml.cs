436bf0098868411dbde2f04d67e6abec0dd27211
Martin Honzátko <u17.405@pslib.cloud>
2020-04-15 21:56:59 +0200

    TempData + PartialView + Basics of Gameon Page (Honzátko)

--- Chmelar_Bielik_Honzatko_Hubicka/Pages/Gameon.cshtml.cs
+++ Chmelar_Bielik_Honzatko_Hubicka/Pages/Gameon.cshtml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
+using Chmelar_Bielik_Honzatko_Hubicka.Helpers;
+using Chmelar_Bielik_Honzatko_Hubicka.Models;
+using Chmelar_Bielik_Honzatko_Hubicka.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +13,27 @@ namespace Chmelar_Bielik_Honzatko_Hubicka
 {
     public class GameonModel : PageModel
     {
+        readonly GameManipulator _gm;
+        readonly GameSessionStorage<Guid> _gss;
+        readonly ApplicationDbContext _db;
+
+        public NavyBattlePiece Piece { get; set; } //For selecting of piece to hit.
+        public Color Color { get; set; } //Color of the cell.
+
+        public GameonModel()
+        {
+            Piece = new NavyBattlePiece();
+            Color = new Color();
+        }
+
         public void OnGet()
         {
 
+        }
+
+        public void OnPostMessage(string text) //For messages.
+        {
+            TempData.AddMessage("messagebox", TempDataExtension.MessageType.success, text);
         }
     }
 }
