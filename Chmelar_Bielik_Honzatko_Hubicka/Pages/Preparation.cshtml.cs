8b586eb0dce74215663a27faa0195376b51a75c0
TomasChmelar <43739768+TomasChmelar@users.noreply.github.com>
2020-04-20 11:50:49 +0200

    boardcss

--- Chmelar_Bielik_Honzatko_Hubicka/Pages/Preparation.cshtml.cs
+++ Chmelar_Bielik_Honzatko_Hubicka/Pages/Preparation.cshtml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
+using Chmelar_Bielik_Honzatko_Hubicka.Helpers;
+using Chmelar_Bielik_Honzatko_Hubicka.Models;
+using Chmelar_Bielik_Honzatko_Hubicka.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +13,23 @@ namespace Chmelar_Bielik_Honzatko_Hubicka
 {
     public class PreparationModel : PageModel
     {
+        readonly GameManipulator _gm;
+        readonly GameSessionStorage<Guid> _gss;
+        readonly ApplicationDbContext _db;
+
+        public NavyBattlePiece Piece { get; set; } //For selecting of piece to hit.
+        public Color Color { get; set; } //Color of the cell.
+
+        public PreparationModel()
+        {
+            Piece = new NavyBattlePiece();
+            Color = new Color();
+        }
+
+        public void OnPostMessage(string text) //For messages.
+        {
+            TempData.AddMessage("messagebox", TempDataExtension.MessageType.success, text);
+        }
         public void OnGet()
         {
 
